6988d574cdb824aaacb01a66bd112d76e3673481
Chavoun Miller <millerchavoun21@gmail.com>
2022-11-05 19:35:30 -0500

    Edited Javascript

---
+++ app.js
@@ -0,0 +1,25 @@
+window.onload=function(){
+	let search = document.getElementsByClassName('btn')[0];
+	var url = "http://localhost:8080/superheroes.php?query=";
+
+	search.addEventListener("click", function(e){
+		e.preventDefault();
+        var req = new XMLHttpRequest();
+		var s = document.getElementById("search").value;
+
+		req.onreadystatechange = function(){
+			if (req.readyState == XMLHttpRequest.DONE) {
+				if (req.status == 200){	
+					var response = req.responseText;
+                    var result = document.getElementById('results')
+					result.innerHTML= response;
+				}else{
+					alert("There was a problem with the request.");
+				}
+			}
+		}
+		console.log(s);
+		req.open('GET',url+s, true);
+		req.send();
+	});
+}
